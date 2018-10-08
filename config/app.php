18b0f639b9b45092b06dea3a4a863d14c91b399a
expert0099 <expertteam009@gmail.com>
2018-10-08 12:14:27 +0530

    cache updated for image display

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/printed_carts/'),
+    'url' => env('APP_URL', 'http://localhost/printedcart/'),
 
     /*
     |--------------------------------------------------------------------------
